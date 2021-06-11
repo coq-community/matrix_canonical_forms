b050651612cb351dac23945104dd546a0cc9ab97
Yves Bertot <Yves.Bertot@inria.fr>
2021-06-11 19:02:23 +0200

    these files must also be included for the development to compile

--- theories/perm_eq_image.v
+++ theories/perm_eq_image.v
@@ -1,4 +1,4 @@
-From mathcomp Require Import all_ssreflect.
+From mathcomp Require Import all_ssreflect all_algebra.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
@@ -32,3 +32,44 @@ by rewrite !size_map.
 Qed.
 
 End Finfun.
+
+Section BigOp.
+
+Variables (T : Type) (idx : T) (op : Monoid.com_law idx).
+
+Lemma sumn_big s : sumn s = (\sum_(i <- s) i)%N.
+Proof.
+elim: s=> /= [|a l ->]; first by rewrite big_nil.
+by rewrite big_cons.
+Qed.
+(***Not in bigop.v and I not found a short way to prove this. ****)
+Lemma big_lift_ord n F j :
+  \big[op/idx]_( i < n.+1 | j != i ) F i = \big[op/idx]_i F (lift j i).
+Proof.
+case: (pickP 'I_n) => [k0 _ | n0]; last first.
+  by rewrite !big1 // => [k /unlift_some[i] | i _]; have:= n0 i.
+rewrite (reindex (lift j)).
+  by apply: eq_bigl=> k; rewrite neq_lift.
+exists (fun k => odflt k0 (unlift j k)) => k; first by rewrite liftK.
+by case/unlift_some=> k' -> ->.
+Qed.
+
+End BigOp.
+
+Section Matrix.
+Import GRing.Theory.
+Local Open Scope ring_scope.
+
+
+Section matrix_Type.
+
+Variable T : Type.
+(**** This lemma is useful to rewrite in a big expression, and it is unsightly
+to do a "have" in a proof for proving that. *********)
+Lemma matrix_comp k l m n (E : 'I_k -> 'I_l -> T) (F : 'I_n -> 'I_k) G :
+  \matrix_(i < n, j < m) ((\matrix_(i0 < k, j0 < l) E i0 j0) (F i) (G j)) =
+  \matrix_(i, j) (E (F i) (G j)).
+Proof. by apply/matrixP=> i j; rewrite !mxE. Qed.
+
+End matrix_Type.
+End Matrix.
